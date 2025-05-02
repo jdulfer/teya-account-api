20611709e1036387e32de2c4a5f7c771831259ad
jdulfer <james.dulfer@gmail.com>
2025-05-02 21:48:20 +0100

    Hello World route

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,13 +1,5 @@
 package com.example
 
-import io.ktor.server.application.*
-
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
-}
-
-fun Application.module() {
-    configureSerialization()
-    configureFrameworks()
-    configureRouting()
 }
